4151e1bf73beac79a9262d06b26f443d08e9f843
Samuelalfonso <samuelalfonzoc@hotmail.com>
2016-05-18 15:18:13 -0430

    Create prueba1.1.py

---
+++ prueba1.1.py
@@ -0,0 +1,8 @@
+# EjercicioProf
+Ejercicio de 2 arreglos
+
+ListaAlf=["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,v,w,x,y,z"]
+ListaNum=["1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20"]
+
+
+Print (listaAlf)  +   (listaNum)
